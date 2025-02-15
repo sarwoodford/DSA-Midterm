aabf501981ed6d95d374db2e5a36f2da01426f24
sara woodford <sarawoodford6@gmail.com>
2025-02-15 16:38:02 -0330

    feat: User class
    - name sttribute and getter for it
    - method to add  atask for User
    - method to mark users task as completed
    - method to display all tasks specific to User

--- src/methods/User.java
+++ src/methods/User.java
@@ -1,4 +1,30 @@
 package methods;
 
 public class User {
+
+    String userName;
+    private TaskList taskList;
+
+    public User(String userName) {
+        this.userName = userName;
+        this.taskList = new TaskList();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void addTask(Task task) {
+        taskList.addTask(task);
+    }
+
+    public void markTaskCompleted(String taskDescription) {
+        taskList.markTaskCompleted(taskDescription);
+    }
+
+    public void displayTasks() {
+        System.out.println(userName + "'s Tasks: ");
+        taskList.displayTasks();
+    }
+
 }
