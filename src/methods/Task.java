9841ed59a319c8212e0c95c5799111b83bda8fa8
sara woodford <sarawoodford6@gmail.com>
2025-02-15 16:14:07 -0330

    feat: Task class
    - sttributes: task description and task status
    - getters and setters for both attributes
    - method to determine if a task is completed or
    not (status = true)
    - method to display task info

--- src/methods/Task.java
+++ src/methods/Task.java
@@ -1,4 +1,37 @@
 package methods;
 
 public class Task {
+    private String taskDescription;
+    private boolean taskStatus;
+
+    public Task(String taskDescription, boolean taskStatus) {
+        this.taskDescription = taskDescription;
+        this.taskStatus = taskStatus;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public boolean getTaskStatus() {
+        return taskStatus;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public void setTaskStatus(boolean taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+
+    public void markTaskComplete() {
+        this.taskStatus = true;
+    }
+
+    public void displayTaskInfo() {
+        String status = taskStatus ? "Complete" : "Not Completed";
+
+        System.out.println("Task Description: " + taskDescription + "Status: " + status);
+    }
 }
